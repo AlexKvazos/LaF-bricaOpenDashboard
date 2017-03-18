59d8436909e7eb6f8c266c39cb0ab3c2ecd2c3c8
AlexKvazos <contact@alexkvazos.com>
2017-03-18 11:06:11 -0600

    Add cool phrases to slack messages

--- src/server/modules/fabrica.js
+++ src/server/modules/fabrica.js
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import Slack from './slack';
+import phrases from './phrases';
 
 class Fabrica {
 
@@ -11,15 +13,27 @@ class Fabrica {
   }
 
   static open() {
+    let day = moment().day();
+
+    let list = phrases[day]['open'];
+    let phrase = list[Math.floor(Math.random()*list.length)];
+
     Slack.send({
       title: 'La Fábrica is now open!',
+      text: phrase,
       color: '#7ed510'
     });
   }
 
   static close() {
+    let day = moment().day();
+
+    let list = phrases[day]['close'];
+    let phrase = list[Math.floor(Math.random()*list.length)];
+
     Slack.send({
       title: 'La Fábrica is now closed!',
+      text: phrase,
       color: '#e9430e'
     });
   }
---
+++ src/server/modules/phrases.js
@@ -0,0 +1,52 @@
+
+/**
+ * Phrases
+ * @type {Object}
+ */
+const phrases = [
+
+  // Sunday
+  {
+    open: [ 'Are we even supposed to open on sundays?' ],
+    close: [ 'See you tomorrow!' ]
+  },
+
+  // Monday
+  {
+    open: [ 'New monday, new week, new goals.' ],
+    close: [ 'Nice work team, we\'ll keep on tomorrow.' ]
+  },
+
+  // Tuesday
+  {
+    open: [ 'Time to hustle!' ],
+    close: [ 'Productive day, wasn\'t it?' ]
+  },
+
+  // Wednesday
+  {
+    open: [ 'You\'re one day closer from your goals, keep on going!' ],
+    close: [ 'Get some rest, you deserve it!' ]
+  },
+
+  // Thursday
+  {
+    open: [ 'Time to put in the work.' ],
+    close: [ 'Way to go team, see you tomorrow!' ]
+  },
+
+  // Friday
+  {
+    open: [ 'It\'s friday now, one last push!' ],
+    close: [ 'Work hard, play hard. Enjoy your weekend!' ]
+  },
+
+  // Sautrday
+  {
+    open: [ 'Hard worker huh? I like that!' ],
+    close: [ 'Get some rest, you earned it!' ]
+  }
+
+];
+
+export default phrases;
